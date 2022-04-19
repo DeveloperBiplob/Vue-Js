fa60550e97e3978f8363eafe1c10bc93a6a5c731
BIPLOB <developerbiplob.bd@gmail.com>
2022-04-19 12:46:32 +0600

    Directives: v-bind

--- Vue Practice/app.js
+++ Vue Practice/app.js
@@ -1,26 +1,33 @@
 new Vue({
     el: '#app',
-    data : {
-        title: 'Hello World!',
-        isItTrue: true,
-        cars: ['Ford', 'Toyota'],
-        robot: {
-            name: 'Franky'
-        }
-        // gretting: ()=> {
-        //     return "Hello World! Function"
-        // }
-    },
-    methods: {
-        // //Old Way--
-        gretting: function(){
-            return 'Hello Biplob'
-        },
+    // data : {
+    //     title: 'Hello World!',
+    //     isItTrue: true,
+    //     cars: ['Ford', 'Toyota'],
+    //     robot: {
+    //         name: 'Franky'
+    //     }
+    //     // gretting: ()=> {
+    //     //     return "Hello World! Function"
+    //     // }
+    // },
+    // methods: {
+    //     // //Old Way--
+    //     gretting: function(){
+    //         return 'Hello Biplob'
+    //     },
 
-        // Es6 --
-        grettingName(){
-            // return 'Hello world'
-            return this.title
-        }
+    //     // Es6 --
+    //     grettingName(){
+    //         // return 'Hello world'
+    //         return this.title
+    //     }
+    // }
+
+    // Directives: V-bind-----//
+    data: {
+        src: "https://picsum.photos/id/237/200/300",
+        alt: "This is a Dog",
+        link: 'http://www.google.com'
     }
 });
