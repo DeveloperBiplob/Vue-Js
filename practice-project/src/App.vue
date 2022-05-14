727f28429c2c66aaa29e365532aa33f724c92e55
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-14 13:23:32 +0600

    Vue Components

--- practice-project/src/App.vue
+++ practice-project/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <App-Navbar></App-Navbar>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+    data(){
+      return {
+        name: 'Biplob',
+        age: 20
+      }
+    },
+    components:{
+      'App-Navbar': Navbar
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+*{
+    padding: 0;
+    margin: 0;
 }
 </style>
---
+++ practice-project/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Contacts</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App-Navbar',
+    data(){
+        return {
+
+        }
+    }
+   
+}
+</script>
+
+<style scoped>
+nav{
+    max-width: 100%;
+    margin: auto;
+    background: rgb(189, 177, 177);
+    color: brown;
+    padding: 20px 0;
+    text-align: center;
+}
+ul li{
+    list-style: none;
+    display: inline-block;
+}
+ul li a{
+    text-decoration: none;
+    padding: 0 20px;
+}
+</style>
