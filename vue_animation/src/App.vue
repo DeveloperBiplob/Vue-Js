ff32ff8ccf6a7b44a299e99f4eabfd5709fb10b9
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-16 11:25:06 +0600

    Vue Animation | Transition

---
+++ vue_animation/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <transition name="appear">
+      <div class="p-2 mb-2 bg-success" v-if="display">Hello</div>
+    </transition>
+    <button @click="display=!display" class="btn btn-success btn-sm">Clck ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  neme: 'App',
+  data() {
+    return {
+      display: false,
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .appear-enter{
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  .appear-enter-active{
+    transition: 3s;
+  }
+  .appear-leave{
+    opacity: 1;
+  }
+  .appear-leave-active{
+    opacity: 0;
+    transition: 3s;
+    transform: translateX(100px);
+  }
+</style>
