95ab62f55fe999f1fd7c566f4e692bc16397fb8f
BIPLOB <developerbiplob.bd@gmail.com>
2022-05-17 11:47:12 +0600

    Vue Scroll Behavior

--- vue_route/src/main.js
+++ vue_route/src/main.js
@@ -9,7 +9,33 @@ Vue.config.productionTip = false
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+
+  // Scroll Behavior use korar karon holo-
+  // kono kata page theke onno akt page jawar somoy oi page er scroll bar ta,
+  // jei position e chilo jei page e jabo oi page o scroll bar ta same positione show kore.
+  // ti ata thik korar jono scroll Behavior ta use korbo.
+
+  // scrollBehavior(){
+  //   return {
+  //     x: 0,
+  //     y: 0
+  //   }
+  // }
+  
+  // scrollBehavior e amara paramiter use korte pari.
+  // ai gulu diye kono page theke kon page e jasche tar shob inpormatin tule ana jay.
+  scrollBehavior(to, from, savedPosition){
+    console.log(from);
+    console.log(to);
+    console.log(savedPosition);
+    
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+
 })
 
 new Vue({
